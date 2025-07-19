be001028d9379fea785db83aea7c74dddb693df8
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-19 11:27:35 -0300

    Configures environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+import config from "./config.ts";
 import { createServer } from "./server.ts";
 
 const server = createServer();
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,11 @@ import type { Request, Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import helmet from "helmet";
+import dotenv from "dotenv";
+import config from "./config";
 
 export const createServer = () => {
+  dotenv.config();
   const app = express();
   app
     .disable("x-powered-by")
@@ -15,7 +18,7 @@ export const createServer = () => {
     .use(helmet());
 
   app.get("/health", (req: Request, res: Response) => {
-    res.json({ ok: true });
+    res.json({ ok: true, environment: config.env });
   });
 
   return app;
