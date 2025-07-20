88985bb0e88cba02653b366f61f1209e9ac46af3
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-20 19:44:56 -0300

    Updates to auth0 handler

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -2,6 +2,7 @@ import type { NextFunction, Request, Response } from "express";
 import config from "../config";
 import { getErrorMessage } from "../utils";
 import CustomError from "../errors/CustomError";
+import { UnauthorizedError } from "express-oauth2-jwt-bearer";
 
 export default function errorHandler(
   error: unknown,
@@ -19,6 +20,16 @@ export default function errorHandler(
       error: {
         message: error.message,
         code: error.code,
+      },
+    });
+    return;
+  }
+
+  if (error instanceof UnauthorizedError) {
+    res.status(error.statusCode).json({
+      error: {
+        message: error.message,
+        code: "code" in error ? error.code : "ERR_AUTH",
       },
     });
     return;
