62b42828793461fc834f87b5abfbcf53483fb5c4
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-24 20:08:53 -0300

    Changes operator of verifyUserExists response

--- src/routes/v1/users/service.ts
+++ src/routes/v1/users/service.ts
@@ -2,11 +2,7 @@ import { getUserByAuth0Id } from "@db/users";
 
 export async function verifyUserExists(auth0_id: string) {
   const user = await getUserByAuth0Id(auth0_id);
-  if (user) {
-    return user;
-  } else {
-    return undefined;
-  }
+  return user ?? undefined;
 }
 
 export async function initializeUser(auth0_id: string) {
