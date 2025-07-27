a5c832a133643f9d0e967cf3b8b017a018208a3f
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-26 23:50:59 -0300

    test: refactor custom error spacing

--- src/errors/CustomError.ts
+++ src/errors/CustomError.ts
@@ -3,15 +3,7 @@ class CustomError<C extends string> extends Error {
   statusCode: number;
   code?: C;
 
-  constructor({
-    message,
-    statusCode,
-    code,
-  }: {
-    message: string;
-    statusCode: number;
-    code?: C;
-  }) {
+  constructor({ message, statusCode, code }: { message: string; statusCode: number; code?: C }) {
     super();
     this.message = message;
     this.statusCode = statusCode;
