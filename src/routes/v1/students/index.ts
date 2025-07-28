0828ffa0026e3c225b4403eb221b9e83e2b87145
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-07-28 01:21:18 -0300

    clean: removes unused route

--- src/routes/v1/students/index.ts
+++ src/routes/v1/students/index.ts
@@ -5,10 +5,6 @@ import { validateStudentController } from "./controller";
 const students: Router = express.Router();
 
 students.use(authenticateUser);
-students.use((req, res, next) => {
-  console.log("Student route hit:", req.method, req.path);
-  next();
-});
 students.post("/validate", validateStudentController);
 
 export default students;
